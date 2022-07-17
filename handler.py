3389a6f2b3fc4528ea3f2e880276344c59896d1b
hemanth28 <hemanth@codeops.tech>
2022-07-17 09:25:17 +0530

    added few files

--- handler.py
+++ handler.py
@@ -1,24 +1,15 @@
+import datetime
 import json
 
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    body = dict(
+        name="Hemanth",
+        DOB="5th April 1998",
+        current_time=datetime.datetime.now().strftime("%c"),
+    )
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
